1fe2e7b13ed3285e89564cb6f0445ffc1496f41f
Dan Peddle <dan@flarework.com>
2015-02-21 19:11:07 +0100

    Initial Commit

---
+++ ping/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ping
+//
+//  Created by Dan Peddle on 21/02/15.
+//  Copyright (c) 2015 Dan Peddle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
